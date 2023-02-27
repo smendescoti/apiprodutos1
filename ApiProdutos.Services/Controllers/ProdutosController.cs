b83c25e49f83940cd8e1765c573dbb2398bd0988
smendescoti <sergio.coti@gmail.com>
2023-02-27 12:51:09 -0300

    first commit

---
+++ ApiProdutos.Services/Controllers/ProdutosController.cs
@@ -0,0 +1,58 @@
+﻿using ApiProdutos.Services.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProdutos.Services.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        /// <summary>
+        /// Serviço para cadastro de produto na API
+        /// </summary>
+        [HttpPost]
+        public IActionResult Post(ProdutosPostModel model)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Serviço para edição de produto na API
+        /// </summary>
+        [HttpPut]
+        public IActionResult Put(ProdutosPutModel model)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Serviço para exclusão de produto na API
+        /// </summary>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid? id)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Serviço para consultar todos os produtos na API
+        /// </summary>
+        [HttpGet]
+        [ProducesResponseType(typeof(List<ProdutosGetModel>), 200)]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Serviço para consultar 1 produto na API
+        /// </summary>
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(ProdutosGetModel), 200)]
+        public IActionResult GetById(Guid? id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ApiProdutos.Services/Models/CategoriasGetModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiProdutos.Services.Models
+{
+    /// <summary>
+    /// Modelo de dados para consulta de categorias
+    /// </summary>
+    public class CategoriasGetModel
+    {
+        public Guid? IdCategoria { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+    }
+}
